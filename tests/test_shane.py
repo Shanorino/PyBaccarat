8ddc0498008d4ab66d5195e2a80fc3a4cbd31b0e
shanorino <laozishixs@gmail.com>
2020-10-27 01:47:36 +0100

    Add logic for checking hit

--- tests/test_shane.py
+++ tests/test_shane.py
@@ -7,19 +7,30 @@ import random
 
 player = Hand()
 banker = Hand()
-print ('给我黑桃1-6!')
+print ('把黑桃1-6洗入牌库!')
 shoe = Shoe([ Card(1,'s'), Card(2,'s'), Card(3,'s'), Card(4,'s'), Card(5,'s'), Card(6,'s') ])
 #shoe = Shoe(1)
 print ('开始洗牌')
 shoe.shuffle()
 for i in range(6):
-    newCard = shoe.deal()
     if (i % 2 == 0):
-        print ('发1张牌: ', newCard, '给闲')
-        player.add(newCard)
+        if (i != 4):
+            newCard = shoe.deal()
+            print ('发1张牌: ', newCard.get_rank(), '给闲')
+            player.add(newCard)
+        elif (player.need_hit(None)):
+            newCard = shoe.deal()
+            print ('发第3张牌: ', newCard.get_rank(), '给闲')
+            player.add(newCard)
     else:
-        print ('发1张牌: ', newCard, '给庄')
-        banker.add(newCard)
+        if (i != 5):
+            newCard = shoe.deal()
+            print ('发1张牌: ', newCard.get_rank(), '给庄')
+            banker.add(newCard)
+        elif (banker.need_hit(player)):
+            newCard = shoe.deal()
+            print ('发第3张牌: ', newCard.get_rank(), '给庄')
+            banker.add(newCard)
 result = player.__cmp__(banker)
 if (result == 0):
     print ('平局')
@@ -27,5 +38,3 @@ elif (result > 0):
     print ('闲赢')
 else:
     print ('庄赢')
-print (player.value())
-print (banker.value())
